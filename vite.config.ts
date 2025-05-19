4fba00952e66715fce5fd164b14caa0a782b8eeb
Arman-2107046 <armanr.rafi@gmail.com>
2025-05-19 19:27:49 +0600

    updated for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
+    base: '/Expense-Tracker-React-TS/', // 👈 Add this line!
   plugins: [react()],
 })
